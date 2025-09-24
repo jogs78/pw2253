18833f7abf02215bc780033a65f11925d86374ff
Jorge Octavio Guzmán Sánchez <jogs78@gmail.com>
2025-09-24 09:28:20 -0600

    Con un scritp

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+def mostrar:
+void mostrar(){}
+*/
+
+function mostrar(){
+ parte = document.getElementById("escolares");
+ parte.style.display = 'block';
+}
+function ocultar(){
+ parte = document.getElementById("escolares");
+ parte.style.display = 'none';
+}
